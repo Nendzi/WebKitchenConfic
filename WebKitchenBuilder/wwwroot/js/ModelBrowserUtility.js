5d435f174231a680b2470f8f4aacdc5d2809a607
Nedeljko Šovljanski <sovljaned@gmail.com>
2020-11-27 21:59:00 +0100

    Add new functionality
    Follow dimension of element and make vertical axis on right places

--- WebKitchenBuilder/wwwroot/js/ModelBrowserUtility.js
+++ WebKitchenBuilder/wwwroot/js/ModelBrowserUtility.js
@@ -6,22 +6,26 @@
 
             if (doorIsOpen) {
                 doorIsOpen = false;
-
+                // elements closing
                 doorChangesState = true;
                 if (allElementsInAssembly[0].substr(0, 6) === "Drawer") {
                     _animateElement(allElementsInAssembly, -0.5, 0.1, "Drawer");
-                } else if (allElementsInAssembly[0].substr(0, 5) === "Doors") {
-                    _animateElement(allElementsInAssembly, -0.5, 0.1, "Doors");
+                } else if (allElementsInAssembly[0].substr(0, 4) === "Leva") {
+                    _animateElement(allElementsInAssembly, -0.5, 0.1, "Leva");
+                } else if (allElementsInAssembly[0].substr(0, 5) === "Desna") {
+                    _animateElement(allElementsInAssembly, -0.5, 0.1, "Desna");
                 }
             }
             else {
                 doorIsOpen = true;
-
+                // elements opening
                 doorChangesState = true;
                 if (allElementsInAssembly[0].substr(0, 6) === "Drawer") {
                     _animateElement(allElementsInAssembly, 0, -0.1, "Drawer");
-                } else if (allElementsInAssembly[0].substr(0, 5) === "Doors") {
-                    _animateElement(allElementsInAssembly, 0, -0.1, "Doors");
+                } else if (allElementsInAssembly[0].substr(0, 4) === "Leva") {
+                    _animateElement(allElementsInAssembly, 0, -0.1, "Leva");
+                } else if (allElementsInAssembly[0].substr(0, 5) === "Desna") {
+                    _animateElement(allElementsInAssembly, 0, -0.1, "Desna");
                 }
             }
             NOP_VIEWER.clearSelection();
@@ -33,7 +37,6 @@ function searchInOwnAssembly(nodeId) {
     var componets = [];
 
     getNeightboreLeafComponentsFromLeaf(NOP_VIEWER, nodeId, function (dbIds) {
-        //console.log('Found ' + dbIds.length + ' leaf nodes');
         if (dbIds[0] == "Main element" || dbIds[0].substr(0, 6) == "Closed") {
             return componets;
         }
@@ -46,7 +49,6 @@ function searchInOwnAssembly(nodeId) {
 }
 
 function getNeightboreLeafComponentsFromLeaf(viewer, selectedId, callback) {
-    var cbCount = 0; // count pending callbacks
     var components = []; // store the results
     var tree; // the instance tree
     var parentNodeId; // id of parant of input (selected) part
@@ -56,7 +58,7 @@ function getNeightboreLeafComponentsFromLeaf(viewer, selectedId, callback) {
         tree = objectTree;
 
         parentNodeId = tree.getNodeParentId(selectedId);
-        parentNodeId = tree.getNodeParentId(parentNodeId); // dva roditelja tj, dedu tražim.
+        parentNodeId = tree.getNodeParentId(parentNodeId); // two parents, I am looking for grandfather
         parentName = tree.getNodeName(parentNodeId);
         components.push(parentName);
         tree.enumNodeChildren(parentNodeId, function (child) {
@@ -69,39 +71,15 @@ function getNeightboreLeafComponentsFromLeaf(viewer, selectedId, callback) {
 doorIsOpen = false;
 doorChangesState = false;
 
-function _openDoors(ids) {
-
-}
-
-function _closeDoors(ids) {
-
-}
-
-function _closeDrawer(ids) {
-
-}
-
-function _openDrawer(ids) {
-
-}
-
-function _disableAnimations() {
-    clearInterval(this._timer);
-}
-
 function _animateElement(ids, startPosition, step, animationSelector) {
     const viewer = this.viewer;
     const it = viewer.model.getData().instanceTree;
-    //console.log(it);
     const axis = new THREE.Vector3(0, 1, 0);
-    //console.log(axis);
     const meshes = [];
 
-    //console.log(ids);
     for (var id of ids) {
         it.enumNodeFragments(id, function (fragId) {
             const mesh = viewer.impl.getFragmentProxy(viewer.model, fragId);
-            //console.log(mesh);
             mesh.scale = new THREE.Vector3(1, 1, 1);
             mesh.quaternion = new THREE.Quaternion(0, 0, 0, 1);
             mesh.position = new THREE.Vector3(0, 0, 0);
@@ -113,42 +91,43 @@ function _animateElement(ids, startPosition, step, animationSelector) {
 
     var _timer = setInterval(function () {
         for (const mesh of meshes) {
+            const halfWidth = viewer.model.getBoundingBox(false);
+            const zAxisXpos = halfWidth.max.x - 1;// 299;
+            const zAxisYpos = 277;
 
-            const posMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(-299, -277, 0));
-            //console.log(posMtrx);
-            const someMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(299, 277, 0));
             const mainMtrx = new THREE.Matrix4();
-            //console.log(mainMtrx);
 
-
-            if (animationSelector === "Doors") {
-                // Animate door
+            if (animationSelector === "Leva") {
+                // Animate left door
+                const posMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(-zAxisXpos, -zAxisYpos, 0));
+                const someMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(zAxisXpos, zAxisYpos, 0));
                 posMtrx.multiply(mainMtrx.makeRotationZ(Math.PI * counter)).multiply(someMtrx);
+                posMtrx.decompose(mesh.position, mesh.quaternion, mesh.scale);
             } else if (animationSelector === "Drawer") {
                 // Animate drawer
+                const posMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(0, 0, 0));
                 posMtrx.makeTranslation(0, 1000 * counter, 0);
-                //posMtrx.multiply(mainMtrx.makeTranslation(0, 1000 * counter, 0)).multiply(someMtrx);
+                posMtrx.decompose(mesh.position, mesh.quaternion, mesh.scale);
+            } else if (animationSelector === "Desna") {
+                // Animate right door
+                const posMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(zAxisXpos, -zAxisYpos, 0));
+                const someMtrx = new THREE.Matrix4().setPosition(new THREE.Vector3(-zAxisXpos, zAxisYpos, 0));
+                posMtrx.multiply(mainMtrx.makeRotationZ(-Math.PI * counter)).multiply(someMtrx);
+                posMtrx.decompose(mesh.position, mesh.quaternion, mesh.scale);
             }
-            //console.log(posMtrx);
-            //mesh.quaternion.setFromAxisAngle(axis, Math.PI * counter);
-            posMtrx.decompose(mesh.position, mesh.quaternion, mesh.scale);
-            //mesh.quaternion.setFromRotationMatrix(posMtrx);
             mesh.updateAnimTransform();
         }
         counter += step;
-        console.log(counter);
         viewer.impl.invalidate(true, true, true);
         if (counter < -0.505) {
             this.doorIsOpen = true;
             this.doorChangesState = false;
             clearInterval(_timer);
-            //this._disableAnimations();
         }
         if (counter > 0.05) {
             this.doorIsOpen = false;
             this.doorChangesState = false;
             clearInterval(_timer);
-            //this._disableAnimations();
         }
     }, 100);
 }
